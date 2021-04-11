1694081df0b1372b66ce00663004aa4a152c5414
Tanzeel <tanzeel.dw@gmail.com>
2021-04-11 18:48:28 -0400

    added login functionality with jwt

--- server/app.js
+++ server/app.js
@@ -5,17 +5,17 @@ const mongoose = require('mongoose');
 
 const morgan = require('morgan');
 
-const jobsRouter = require('./controllers/jobs');
-const usersRouter = require('./controllers/users');
-
 const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.use('/api/v1/jobs', jobsRouter);
-app.use('/api/v1/users', usersRouter);
+app.use('/api/v1/jobs', require('./controllers/jobs'));
+
+app.use('/api/v1/users', require('./controllers/users'));
+
+app.use('/api/v1/login', require('./controllers/login'));
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true,
---
+++ server/controllers/login.js
@@ -0,0 +1,41 @@
+const loginRouter = require('express').Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
+
+loginRouter.post('/', async (request, response) => {
+  const { body } = request;
+  const user = await User.findOne({ username: body.username });
+
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(body.password, user.passwordHash);
+
+  if (!(user && passwordCorrect)) {
+    return response
+      .status(401)
+      .json({
+        statusCode: 401,
+        status: 'Unauthorized',
+        message: 'Incorrect username or password',
+      });
+  }
+
+  const userForToken = {
+    username: user.username,
+    name: user.name,
+    id: user._id.toString(),
+  };
+
+  const token = await jwt.sign(userForToken, process.env.JWT_SECRET);
+
+  return response.status(200).json({
+    statusCode: 200,
+    status: 'OK',
+    message: `Logged in as ${user.username}`,
+    token,
+  });
+});
+
+module.exports = loginRouter;
