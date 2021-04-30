5e9bfae2251ad4849d7afcc706e2001af48db68c
Tanzeel <tanzeel.dw@gmail.com>
2021-04-30 10:49:27 -0400

    Initialized client with vanilla React

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { Reset } from 'styled-reset';
+
+const App = () => (
+  <>
+    <Reset />
+    <div className="App">
+      <header className="App-header">
+        <h1>Testing</h1>
+      </header>
+    </div>
+  </>
+
+);
+
+export default App;
