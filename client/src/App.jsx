acc8bceb6f1a368a5be60d27bb4c1e2b9888c827
Tanzeel <tanzeel.dw@gmail.com>
2021-05-16 16:48:37 -0400

    sign in button created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 // import LandingPage from './components/LandingPage';
+import AuthButton from './components/AuthButton/AuthButton';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <AuthButton label="Test" />
+    </div>
   );
 }
 
---
+++ client/src/components/AuthButton/AuthButton.jsx
@@ -0,0 +1,11 @@
+import styles from './AuthButton.module.css';
+
+function AuthButton({ label }) {
+  return (
+    <button onClick={() => console.log('Boink!')} className={styles.AuthButton} type="button">
+      {label}
+    </button>
+  );
+}
+
+export default AuthButton;
